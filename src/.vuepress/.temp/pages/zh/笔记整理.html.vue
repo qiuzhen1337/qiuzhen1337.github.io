d34695740256ab6cf3ed230cdc48c1d15ff6e49e
qiuzhen1337 <qiuzhenqzzz@gmail.com>
2023-08-06 09:36:12 +0800

    完善笔记

---
+++ src/.vuepress/.temp/pages/zh/笔记整理.html.vue
@@ -0,0 +1,73 @@
+<template><div><h1 id="笔记整理" tabindex="-1"><a class="header-anchor" href="#笔记整理" aria-hidden="true">#</a> 笔记整理</h1>
+<h2 id="网站本地启动" tabindex="-1"><a class="header-anchor" href="#网站本地启动" aria-hidden="true">#</a> 网站本地启动</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230728072626503.png" alt="image-20230728072626503" tabindex="0" loading="lazy"><figcaption>image-20230728072626503</figcaption></figure>
+<figure><img src="@source/zh/笔记整理.assets/image-20230728073302956.png" alt="image-20230728073302956" tabindex="0" loading="lazy"><figcaption>image-20230728073302956</figcaption></figure>
+<h2 id="修改网页名称、头像-左上角" tabindex="-1"><a class="header-anchor" href="#修改网页名称、头像-左上角" aria-hidden="true">#</a> 修改网页名称、头像（左上角）</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230728074530741.png" alt="image-20230728074530741" tabindex="0" loading="lazy"><figcaption>image-20230728074530741</figcaption></figure>
+<figure><img src="@source/zh/笔记整理.assets/image-20230728074842642.png" alt="image-20230728074842642" tabindex="0" loading="lazy"><figcaption>image-20230728074842642</figcaption></figure>
+<h2 id="设置网页关键词" tabindex="-1"><a class="header-anchor" href="#设置网页关键词" aria-hidden="true">#</a> 设置网页关键词</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230728075905817.png" alt="image-20230728075905817" tabindex="0" loading="lazy"><figcaption>image-20230728075905817</figcaption></figure>
+<h2 id="社交平台汇总" tabindex="-1"><a class="header-anchor" href="#社交平台汇总" aria-hidden="true">#</a> 社交平台汇总</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230728081749232.png" alt="image-20230728081749232" tabindex="0" loading="lazy"><figcaption>image-20230728081749232</figcaption></figure>
+<h2 id="查看部署-仓库里" tabindex="-1"><a class="header-anchor" href="#查看部署-仓库里" aria-hidden="true">#</a> 查看部署（仓库里）</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230728082112750.png" alt="image-20230728082112750" tabindex="0" loading="lazy"><figcaption>image-20230728082112750</figcaption></figure>
+<h2 id="部署" tabindex="-1"><a class="header-anchor" href="#部署" aria-hidden="true">#</a> 部署</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230728082422411.png" alt="image-20230728082422411" tabindex="0" loading="lazy"><figcaption>image-20230728082422411</figcaption></figure>
+<h2 id="脚注" tabindex="-1"><a class="header-anchor" href="#脚注" aria-hidden="true">#</a> 脚注</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230729071629597.png" alt="image-20230729071629597" tabindex="0" loading="lazy"><figcaption>image-20230729071629597</figcaption></figure>
+<h2 id="左上角" tabindex="-1"><a class="header-anchor" href="#左上角" aria-hidden="true">#</a> (左上角)</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230729072117304.png" alt="image-20230729072117304" tabindex="0" loading="lazy"><figcaption>image-20230729072117304</figcaption></figure>
+<h2 id="跳转仓库" tabindex="-1"><a class="header-anchor" href="#跳转仓库" aria-hidden="true">#</a> 跳转仓库</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230729072505222.png" alt="image-20230729072505222" tabindex="0" loading="lazy"><figcaption>image-20230729072505222</figcaption></figure>
+<figure><img src="@source/zh/笔记整理.assets/image-20230729073213780.png" alt="image-20230729073213780" tabindex="0" loading="lazy"><figcaption>image-20230729073213780</figcaption></figure>
+<h2 id="修改网页头像" tabindex="-1"><a class="header-anchor" href="#修改网页头像" aria-hidden="true">#</a> 修改网页头像</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230729074919344.png" alt="image-20230729074919344" tabindex="0" loading="lazy"><figcaption>image-20230729074919344</figcaption></figure>
+<h2 id="修改网页头像部分内容" tabindex="-1"><a class="header-anchor" href="#修改网页头像部分内容" aria-hidden="true">#</a> 修改网页头像部分内容</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230729075619564.png" alt="image-20230729075619564" tabindex="0" loading="lazy"><figcaption>image-20230729075619564</figcaption></figure>
+<h2 id="保存网页本地修改内容" tabindex="-1"><a class="header-anchor" href="#保存网页本地修改内容" aria-hidden="true">#</a> 保存网页本地修改内容</h2>
+<p>command+s</p>
+<h2 id="快速截图" tabindex="-1"><a class="header-anchor" href="#快速截图" aria-hidden="true">#</a> 快速截图</h2>
+<p>option+1+enter</p>
+<h2 id="在后台管理评论" tabindex="-1"><a class="header-anchor" href="#在后台管理评论" aria-hidden="true">#</a> 在后台管理评论</h2>
+<p><a href="https://mycomment.com/ui" target="_blank" rel="noopener noreferrer">https://mycomment.com/ui<ExternalLinkIcon/></a></p>
+<h2 id="如何重新部署" tabindex="-1"><a class="header-anchor" href="#如何重新部署" aria-hidden="true">#</a> 如何重新部署</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230804212954572.png" alt="image-20230804212954572" tabindex="0" loading="lazy"><figcaption>image-20230804212954572</figcaption></figure>
+<figure><img src="@source/zh/笔记整理.assets/image-20230804213038136.png" alt="image-20230804213038136" tabindex="0" loading="lazy"><figcaption>image-20230804213038136</figcaption></figure>
+<figure><img src="@source/zh/笔记整理.assets/image-20230804213220737.png" alt="image-20230804213220737" tabindex="0" loading="lazy"><figcaption>image-20230804213220737</figcaption></figure>
+<figure><img src="@source/zh/笔记整理.assets/image-20230804213316228.png" alt="image-20230804213316228" tabindex="0" loading="lazy"><figcaption>image-20230804213316228</figcaption></figure>
+<figure><img src="@source/zh/笔记整理.assets/image-20230804213406169.png" alt="image-20230804213406169" tabindex="0" loading="lazy"><figcaption>image-20230804213406169</figcaption></figure>
+<p>image-20230801080913326</p>
+<figure><img src="@source/zh/笔记整理.assets/image-20230801080827337.png" alt="image-20230801080750944" tabindex="0" loading="lazy"><figcaption>image-20230801080750944</figcaption></figure>
+<figure><img src="@source/zh/笔记整理.assets/image-20230801081057344.png" alt="image-20230801081057344" tabindex="0" loading="lazy"><figcaption>image-20230801081057344</figcaption></figure>
+<figure><img src="@source/zh/笔记整理.assets/image-20230804215344739.png" alt="image-20230804215344739" tabindex="0" loading="lazy"><figcaption>image-20230804215344739</figcaption></figure>
+<figure><img src="@source/zh/笔记整理.assets/image-20230801081142166.png" alt="image-20230801081142166" tabindex="0" loading="lazy"><figcaption>image-20230801081142166</figcaption></figure>
+<p><a href="https://theme-hope.vuejs.press/zh/guide/feature/comment.html#%E5%87%86%E5%A4%87%E5%B7%A5%E4%BD%9C" target="_blank" rel="noopener noreferrer">https://theme-hope.vuejs.press/zh/guide/feature/comment.html#准备工作<ExternalLinkIcon/></a></p>
+<h2 id="评论区" tabindex="-1"><a class="header-anchor" href="#评论区" aria-hidden="true">#</a> 评论区</h2>
+<p><a href="https://mycomment.weonlygettoday.com" target="_blank" rel="noopener noreferrer">https://mycomment.weonlygettoday.com<ExternalLinkIcon/></a></p>
+<h2 id="管理评论区" tabindex="-1"><a class="header-anchor" href="#管理评论区" aria-hidden="true">#</a> 管理评论区</h2>
+<p><a href="https://mycomment.weonlygettoday.com/ui" target="_blank" rel="noopener noreferrer">https://mycomment.weonlygettoday.com/ui<ExternalLinkIcon/></a></p>
+<h2 id="注释" tabindex="-1"><a class="header-anchor" href="#注释" aria-hidden="true">#</a> 注释</h2>
+<p>选中+command /</p>
+<h2 id="打开网页内容的路径" tabindex="-1"><a class="header-anchor" href="#打开网页内容的路径" aria-hidden="true">#</a> 打开网页内容的路径</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230805072317873.png" alt="image-20230805072317873" tabindex="0" loading="lazy"><figcaption>image-20230805072317873</figcaption></figure>
+<h2 id="如何快速重启" tabindex="-1"><a class="header-anchor" href="#如何快速重启" aria-hidden="true">#</a> 如何快速重启</h2>
+<p>向上键+enter</p>
+<h2 id="修改主题色" tabindex="-1"><a class="header-anchor" href="#修改主题色" aria-hidden="true">#</a> 修改主题色</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230805080959712.png" alt="image-20230805080959712" tabindex="0" loading="lazy"><figcaption>image-20230805080959712</figcaption></figure>
+<h2 id="用户可选的主题色" tabindex="-1"><a class="header-anchor" href="#用户可选的主题色" aria-hidden="true">#</a> 用户可选的主题色</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230805081047652.png" alt="image-20230805081047652" tabindex="0" loading="lazy"><figcaption>image-20230805081047652</figcaption></figure>
+<h2 id="好用的图标库" tabindex="-1"><a class="header-anchor" href="#好用的图标库" aria-hidden="true">#</a> 好用的图标库</h2>
+<p><a href="https://www.iconfont.cn/" target="_blank" rel="noopener noreferrer">https://www.iconfont.cn/<ExternalLinkIcon/></a></p>
+<h2 id="设置-修改个人简介" tabindex="-1"><a class="header-anchor" href="#设置-修改个人简介" aria-hidden="true">#</a> 设置/修改个人简介</h2>
+<figure><img src="@source/zh/笔记整理.assets/image-20230805081704726.png" alt="image-20230805081704726" tabindex="0" loading="lazy"><figcaption>image-20230805081704726</figcaption></figure>
+<h2 id="快速把多个页面并到一起" tabindex="-1"><a class="header-anchor" href="#快速把多个页面并到一起" aria-hidden="true">#</a> 快速把多个页面并到一起</h2>
+<p>command+n</p>
+<figure><img src="@source/zh/笔记整理.assets/image-20230806085623481.png" alt="image-20230806085623481" tabindex="0" loading="lazy"><figcaption>image-20230806085623481</figcaption></figure>
+<figure><img src="@source/zh/笔记整理.assets/image-20230806092154452.png" alt="image-20230806092154452" tabindex="0" loading="lazy"><figcaption>image-20230806092154452</figcaption></figure>
+<figure><img src="@source/zh/笔记整理.assets/image-20230806092232975.png" alt="image-20230806092232975" tabindex="0" loading="lazy"><figcaption>image-20230806092232975</figcaption></figure>
+<figure><img src="@source/zh/笔记整理.assets/image-20230806092340374.png" alt="image-20230806092340374" tabindex="0" loading="lazy"><figcaption>image-20230806092340374</figcaption></figure>
+<h2 id="如何打出淡色框" tabindex="-1"><a class="header-anchor" href="#如何打出淡色框" aria-hidden="true">#</a> 如何打出淡色框</h2>
+<p>三个- + enter</p>
+</div></template>
+
+
