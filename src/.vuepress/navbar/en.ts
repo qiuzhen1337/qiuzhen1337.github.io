fe07524690bc966ad47fbb48d2997817023b2017
qiuzhen1337 <qiuzhenqzzz@gmail.com>
2023-08-20 07:58:18 +0800

    修改了英文菜单栏

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -1,52 +1,35 @@
 import { navbar } from "vuepress-theme-hope";
 
-export const enNavbar = navbar([
-  "/",
-  "/demo/",
-  {
-    text: "Posts",
-    icon: "pen-to-square",
-    prefix: "/posts/",
+export const zhNavbar = navbar([
+  { text: "accumulating", 
+    icon: "#",
     children: [
-      {
-        text: "Apple",
-        icon: "pen-to-square",
-        prefix: "apple/",
-        children: [
-          { text: "Apple1", icon: "pen-to-square", link: "1" },
-          { text: "Apple2", icon: "pen-to-square", link: "2" },
-          "3",
-          "4",
-        ],
-      },
-      {
-        text: "Banana",
-        icon: "pen-to-square",
-        prefix: "banana/",
-        children: [
-          {
-            text: "Banana 1",
-            icon: "pen-to-square",
-            link: "1",
-          },
-          {
-            text: "Banana 2",
-            icon: "pen-to-square",
-            link: "2",
-          },
-          "3",
-          "4",
-        ],
-      },
-      { text: "Cherry", icon: "pen-to-square", link: "cherry" },
-      { text: "Dragon Fruit", icon: "pen-to-square", link: "dragonfruit" },
-      "tomato",
-      "strawberry",
+      { text: "year", icon: "#", link: "#"},
+      { text: "month", icon: "#", link: "#"},
+      { text: "day", icon: "#", link: "#"},
     ],
   },
-  {
-    text: "V2 Docs",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/",
-  },
+  { text: "skill sharing", 
+  icon: "#",
+  children: [
+    { text: "programming", icon: "#", link: "#"},
+    { text: "necessary for Generation Z", icon: "#", link: "#"},
+    { text: "devotion & promotion", icon: "#", link: "#"},
+  ],
+},
+  { text: "some whimsy", 
+  icon: "#",
+  children: [
+    { text: "current affairs insights", icon: "#", link: "#"},
+    { text: "individual thinking", icon: "#", link: "#"},
+    { text: "reading notes", icon: "#", link: "#"},
+  ],
+},
+  { text: "about me", 
+  icon: "-sportgirl",
+  children: [
+    { text: "personal introduction", icon: "#", link: "/zh/intro.html"},
+    { text: "activity experience", icon: "#", link: "#"},
+  ],
+},
 ]);
