9022f7e645d824a4439162331c12e1dce1f35ae5
qiuzhen1337 <qiuzhenqzzz@gmail.com>
2023-09-03 09:52:39 +0800

    写了年规划，月规划

--- src/.vuepress/.temp/pages/column/accumulating/Month/2023.9.html.vue
+++ src/.vuepress/.temp/pages/column/accumulating/Month/2023.9.html.vue
@@ -7,15 +7,19 @@
 <li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-3" disabled="disabled"><label class="task-list-item-label" for="task-item-3"> 口语起步</label></li>
 <li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-4" disabled="disabled"><label class="task-list-item-label" for="task-item-4"> 每天看BBC半小时+跟读 共一小时</label></li>
 </ul>
+<h3 id="cma" tabindex="-1"><a class="header-anchor" href="#cma" aria-hidden="true">#</a> CMA</h3>
+<ul class="task-list-container">
+<li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-5" disabled="disabled"><label class="task-list-item-label" for="task-item-5"> 一遍刷完</label></li>
+</ul>
 <h3 id="课内" tabindex="-1"><a class="header-anchor" href="#课内" aria-hidden="true">#</a> 课内</h3>
 <ul class="task-list-container">
-<li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-5" disabled="disabled"><label class="task-list-item-label" for="task-item-5"> 不拖大作业 布置下来后想好什么时候做</label></li>
-<li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-6" disabled="disabled"><label class="task-list-item-label" for="task-item-6"> 不拉下任何一门课</label></li>
-<li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-7" disabled="disabled"><label class="task-list-item-label" for="task-item-7"> 熟练纪念馆讲解稿</label></li>
+<li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-6" disabled="disabled"><label class="task-list-item-label" for="task-item-6"> 不拖大作业 布置下来后想好什么时候做</label></li>
+<li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-7" disabled="disabled"><label class="task-list-item-label" for="task-item-7"> 不拉下任何一门课</label></li>
+<li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-8" disabled="disabled"><label class="task-list-item-label" for="task-item-8"> 熟练纪念馆讲解稿</label></li>
 </ul>
 <h3 id="身材管理" tabindex="-1"><a class="header-anchor" href="#身材管理" aria-hidden="true">#</a> 身材管理</h3>
 <ul class="task-list-container">
-<li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-8" disabled="disabled"><label class="task-list-item-label" for="task-item-8"> 9.15 100斤</label></li>
+<li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-9" disabled="disabled"><label class="task-list-item-label" for="task-item-9"> 9.15 100斤</label></li>
 </ul>
 </div></template>
 
