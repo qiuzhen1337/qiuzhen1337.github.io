c0decdfe57a31c5ad4cc18ad9c5ec35893ab1726
qiuzhen1337 <139850753+qiuzhen1337@users.noreply.github.com>
2023-09-13 00:39:37 +0000

    Deploying to gh-pages from @ qiuzhen1337/qiuzhen1337.github.io@6bfbec388df7072979c224a85188142e10b13d25 🚀

---
+++ assets/article-writing.html-f9df547d.js
@@ -0,0 +1,119 @@
+import{_ as m}from"./plugin-vue_export-helper-c27b6911.js";import{r as l,o as h,c as b,b as d,w as s,n as g,g as _,d as n,e as i,a as t}from"./app-33c021bf.js";const x="/assets/image-20230901082027927-ab8544e3.png",f="/assets/image-20230901085929845-58a49cf1.png",k="/assets/image-20230901090901403-8f4d6379.png",y="/assets/image-20230902082141485-d21afc83.png",w={},j=t('<h2 id="_1标题编写" tabindex="-1"><a class="header-anchor" href="#_1标题编写" aria-hidden="true">#</a> 1标题编写</h2><h2 id="标题二" tabindex="-1"><a class="header-anchor" href="#标题二" aria-hidden="true">#</a> 标题二</h2><h3 id="标题三" tabindex="-1"><a class="header-anchor" href="#标题三" aria-hidden="true">#</a> 标题三</h3><p>……</p><h6 id="标题六" tabindex="-1"><a class="header-anchor" href="#标题六" aria-hidden="true">#</a> 标题六</h6><h2 id="_2图片" tabindex="-1"><a class="header-anchor" href="#_2图片" aria-hidden="true">#</a> 2图片</h2><figure><img src="'+x+'" alt="image-20230901082027927" tabindex="0" loading="lazy"><figcaption>image-20230901082027927</figcaption></figure><h2 id="_3代码" tabindex="-1"><a class="header-anchor" href="#_3代码" aria-hidden="true">#</a> 3代码</h2><p>英文输入法下 “1”左边按键，按三次，并输入编程语言的类型，然后回车。</p><div class="language-python line-numbers-mode" data-ext="py"><pre class="language-python"><code>```python\n</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="_4任务列表" tabindex="-1"><a class="header-anchor" href="#_4任务列表" aria-hidden="true">#</a> 4任务列表</h2><ul class="task-list-container"><li class="task-list-item"><input type="checkbox" class="task-list-item-checkbox" id="task-item-0" checked="checked" disabled="disabled"><label class="task-list-item-label" for="task-item-0"> 减号+空格+英文中括号+x+空格 必须要在每行顶格 注意- [x]</label></li></ul><h2 id="_5无序列表" tabindex="-1"><a class="header-anchor" href="#_5无序列表" aria-hidden="true">#</a> 5无序列表</h2><ul><li>减号+空格</li></ul><h2 id="_6有序列表" tabindex="-1"><a class="header-anchor" href="#_6有序列表" aria-hidden="true">#</a> 6有序列表</h2><ol><li>数字+.+空格</li><li></li></ol><h2 id="_7选项卡" tabindex="-1"><a class="header-anchor" href="#_7选项卡" aria-hidden="true">#</a> 7选项卡</h2>',17),z=n("p",null,"123",-1),A=n("p",null,"abc",-1),B=t('<h2 id="_8代码块分组" tabindex="-1"><a class="header-anchor" href="#_8代码块分组" aria-hidden="true">#</a> 8代码块分组</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>:::code-tabs\n@tab code1\n```python\nprint(&#39;a&#39;)\n```\n@tab code2\n```python\nprint(&#39;b&#39;)\n```\n:::\n</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>在代码块中只是为了内部显示</p>',3),E=n("div",{class:"language-python line-numbers-mode","data-ext":"py"},[n("pre",{class:"language-python"},[n("code",null,[n("span",{class:"token keyword"},"print"),n("span",{class:"token punctuation"},"("),n("span",{class:"token string"},"'a'"),n("span",{class:"token punctuation"},")"),i(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"})])],-1),C=n("div",{class:"language-python line-numbers-mode","data-ext":"py"},[n("pre",{class:"language-python"},[n("code",null,[n("span",{class:"token keyword"},"print"),n("span",{class:"token punctuation"},"("),n("span",{class:"token string"},"'b'"),n("span",{class:"token punctuation"},")"),i(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"})])],-1),P=n("h2",{id:"_9图表",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#_9图表","aria-hidden":"true"},"#"),i(" 9图表")],-1),V=n("h2",{id:"_10数学语法",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#_10数学语法","aria-hidden":"true"},"#"),i(" 10数学语法")],-1),T=n("div",{style:{"text-align":"center"}},[n("p",null,[n("span",{class:"katex"},[n("span",{class:"katex-mathml"},[n("math",{xmlns:"http://www.w3.org/1998/Math/MathML"},[n("semantics",null,[n("mrow",null,[n("mstyle",{mathsize:"1.2em"},[n("mfrac",null,[n("msup",null,[n("mi",null,"x"),n("mn",null,"2")]),n("mrow",null,[n("mi",null,"x"),n("mo",null,"+"),n("mi",null,"y")])])])]),n("annotation",{encoding:"application/x-tex"},"\\large\\frac{x^2}{x+y}")])])]),n("span",{class:"katex-html","aria-hidden":"true"},[n("span",{class:"base"},[n("span",{class:"strut",style:{height:"1.7738em","vertical-align":"-0.5696em"}}),n("span",{class:"mord sizing reset-size6 size7"},[n("span",{class:"mopen nulldelimiter sizing reset-size7 size6"}),n("span",{class:"mfrac"},[n("span",{class:"vlist-t vlist-t2"},[n("span",{class:"vlist-r"},[n("span",{class:"vlist",style:{height:"1.0036em"}},[n("span",{style:{top:"-2.655em"}},[n("span",{class:"pstrut",style:{height:"3em"}}),n("span",{class:"sizing reset-size7 size4 mtight"},[n("span",{class:"mord mtight"},[n("span",{class:"mord mathnormal mtight"},"x"),n("span",{class:"mbin mtight"},"+"),n("span",{class:"mord mathnormal mtight",style:{"margin-right":"0.03588em"}},"y")])])]),n("span",{style:{top:"-3.23em"}},[n("span",{class:"pstrut",style:{height:"3em"}}),n("span",{class:"frac-line",style:{"border-bottom-width":"0.04em"}})]),n("span",{style:{top:"-3.394em"}},[n("span",{class:"pstrut",style:{height:"3em"}}),n("span",{class:"sizing reset-size7 size4 mtight"},[n("span",{class:"mord mtight"},[n("span",{class:"mord mtight"},[n("span",{class:"mord mathnormal mtight"},"x"),n("span",{class:"msupsub"},[n("span",{class:"vlist-t"},[n("span",{class:"vlist-r"},[n("span",{class:"vlist",style:{height:"0.9143em"}},[n("span",{style:{top:"-3.031em","margin-right":"0.0625em"}},[n("span",{class:"pstrut",style:{height:"2.6em"}}),n("span",{class:"sizing reset-size4 size2 mtight"},[n("span",{class:"mord mtight"},"2")])])])])])])])])])])]),n("span",{class:"vlist-s"},"​")]),n("span",{class:"vlist-r"},[n("span",{class:"vlist",style:{height:"0.4746em"}},[n("span")])])])]),n("span",{class:"mclose nulldelimiter sizing reset-size7 size6"})])])])])])],-1),N=n("h2",{id:"_11卡片功能-跳转链接",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#_11卡片功能-跳转链接","aria-hidden":"true"},"#"),i(" 11卡片功能（跳转链接）")],-1),D=n("figure",null,[n("img",{src:f,alt:"image-20230901085929845",tabindex:"0",loading:"lazy"}),n("figcaption",null,"image-20230901085929845")],-1),F=t('<h2 id="_12访达内显示隐藏文件" tabindex="-1"><a class="header-anchor" href="#_12访达内显示隐藏文件" aria-hidden="true">#</a> 12访达内显示隐藏文件</h2><p>command+shift+。</p><figure><img src="'+k+'" alt="image-20230901090901403" tabindex="0" loading="lazy"><figcaption>image-20230901090901403</figcaption></figure><p>所有图片放到public里面，/代表public起步</p><h2 id="_13-b站插入" tabindex="-1"><a class="header-anchor" href="#_13-b站插入" aria-hidden="true">#</a> 13 b站插入</h2>',5),L=n("figure",null,[n("img",{src:y,alt:"image-20230902082141485",tabindex:"0",loading:"lazy"}),n("figcaption",null,"image-20230902082141485")],-1),M=n("h2",{id:"_14-pdf插入",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#_14-pdf插入","aria-hidden":"true"},"#"),i(" 14 PDF插入")],-1),q=t(`<p>写public下面的路径</p><h2 id="_15自定义容器" tabindex="-1"><a class="header-anchor" href="#_15自定义容器" aria-hidden="true">#</a> 15自定义容器</h2><div class="hint-container info"><p class="hint-container-title">温馨提示</p><p>大家好，我是仇真</p></div><div class="hint-container note"><p class="hint-container-title">注释</p><p>注释容器</p></div><div class="hint-container tip"><p class="hint-container-title">我是你的提示</p><p>提示容器</p></div><p>更多容器</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>::: info
+
+信息容器。
+
+:::
+
+::: note
+
+注释容器。
+
+:::
+
+::: tip
+
+提示容器
+
+:::
+
+::: warning
+
+警告容器
+
+:::
+
+::: danger
+
+危险容器
+
+:::
+
+::: details
+
+详情容器
+
+:::
+
+::: info 自定义标题
+
+一个有 \`代码\` 和 [链接](#演示) 的信息容器。
+
+\`\`\`js
+const a = 1;
+\`\`\`
+
+:::
+
+::: note 自定义标题
+
+一个有 \`代码\` 和 [链接](#演示) 的注释容器。
+
+\`\`\`js
+const a = 1;
+\`\`\`
+
+:::
+
+::: tip 自定义标题
+
+一个有 \`代码\` 和 [链接](#演示) 的提示容器。
+
+\`\`\`js
+const a = 1;
+\`\`\`
+
+:::
+
+::: warning 自定义标题
+
+一个有 \`代码\` 和 [链接](#演示) 的警告容器。
+
+\`\`\`js
+const a = 1;
+\`\`\`
+
+:::
+
+::: danger 自定义标题
+
+一个有 \`代码\` 和 [链接](#演示) 的危险容器。
+
+\`\`\`js
+const a = 1;
+\`\`\`
+
+:::
+
+::: details 自定义标题
+
+一个有 \`代码\` 和 [链接](#演示) 的详情容器。
+
+\`\`\`js
+const a = 1;
+\`\`\`
+
+:::
+
+::: info 自定义信息
+:::
+
+::: note 自定义注释
+:::
+
+::: tip 自定义提示
+:::
+
+::: warning 自定义警告
+:::
+
+::: danger 自定义危险
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="hint-container info"><p class="hint-container-title">相关信息</p><p>信息容器。</p></div><div class="hint-container note"><p class="hint-container-title">注</p><p>注释容器。</p></div><div class="hint-container tip"><p class="hint-container-title">提示</p><p>提示容器</p></div><div class="hint-container warning"><p class="hint-container-title">注意</p><p>警告容器</p></div><div class="hint-container danger"><p class="hint-container-title">警告</p><p>危险容器</p></div><details class="hint-container details"><summary>详情</summary><p>详情容器</p></details><div class="hint-container info"><p class="hint-container-title">自定义标题</p><p>一个有 <code>代码</code> 和 <a href="#%E6%BC%94%E7%A4%BA">链接</a> 的信息容器。</p><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></div><div class="hint-container note"><p class="hint-container-title">自定义标题</p><p>一个有 <code>代码</code> 和 <a href="#%E6%BC%94%E7%A4%BA">链接</a> 的注释容器。</p><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></div><div class="hint-container tip"><p class="hint-container-title">自定义标题</p><p>一个有 <code>代码</code> 和 <a href="#%E6%BC%94%E7%A4%BA">链接</a> 的提示容器。</p><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></div><div class="hint-container warning"><p class="hint-container-title">自定义标题</p><p>一个有 <code>代码</code> 和 <a href="#%E6%BC%94%E7%A4%BA">链接</a> 的警告容器。</p><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></div><div class="hint-container danger"><p class="hint-container-title">自定义标题</p><p>一个有 <code>代码</code> 和 <a href="#%E6%BC%94%E7%A4%BA">链接</a> 的危险容器。</p><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></div><details class="hint-container details"><summary>自定义标题</summary><p>一个有 <code>代码</code> 和 <a href="#%E6%BC%94%E7%A4%BA">链接</a> 的详情容器。</p><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></details><div class="hint-container info"><p class="hint-container-title">自定义信息</p></div><div class="hint-container note"><p class="hint-container-title">自定义注释</p></div><div class="hint-container tip"><p class="hint-container-title">自定义提示</p></div><div class="hint-container warning"><p class="hint-container-title">自定义警告</p></div><div class="hint-container danger"><p class="hint-container-title">自定义危险</p></div><h2 id="_16自定义对齐" tabindex="-1"><a class="header-anchor" href="#_16自定义对齐" aria-hidden="true">#</a> 16自定义对齐</h2><div style="text-align:left;"><p>左对齐的内容</p></div><div style="text-align:center;"><p>居中的内容</p></div><div style="text-align:right;"><p>右对齐的内容</p><div style="text-align:justify;"><p>两端对齐的内容</p></div></div><h2 id="_17插入链接" tabindex="-1"><a class="header-anchor" href="#_17插入链接" aria-hidden="true">#</a> 17插入链接</h2>`,29),I={href:"https://weonlygettoday.com",target:"_blank",rel:"noopener noreferrer"},G=n("h2",{id:"_18详细信息展开",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#_18详细信息展开","aria-hidden":"true"},"#"),i(" 18详细信息展开")],-1),R=n("details",{class:"hint-container details"},[n("summary",null,"详细信息"),n("p",null,"xxxxxxx")],-1);function S(W,H){const c=l("Tabs"),r=l("CodeTabs"),v=l("VPCard"),o=l("BiliBili"),p=l("PDF"),u=l("ExternalLinkIcon");return h(),b("div",null,[j,d(c,{id:"63",data:[{id:"xxxx"},{id:"xx"}]},{title0:s(({value:e,isActive:a})=>[i("xxxx")]),title1:s(({value:e,isActive:a})=>[i("xx")]),tab0:s(({value:e,isActive:a})=>[z]),tab1:s(({value:e,isActive:a})=>[A]),_:1}),B,d(r,{id:"82",data:[{id:"code1"},{id:"code2"}]},{title0:s(({value:e,isActive:a})=>[i("code1")]),title1:s(({value:e,isActive:a})=>[i("code2")]),tab0:s(({value:e,isActive:a})=>[E]),tab1:s(({value:e,isActive:a})=>[C]),_:1}),P,V,T,N,D,d(v,g(_({title:"qz",desc:"仇真",logo:"/logo.png",color:"rgba(253,230,138,0.15)"})),null,16),F,d(o,{bvid:"BV1wG411o7nq"}),L,M,d(p,{url:"/sample.pdf"}),q,n("p",null,[n("a",I,[i("我的网站"),d(u)])]),G,R])}const O=m(w,[["render",S],["__file","article-writing.html.vue"]]);export{O as default};
